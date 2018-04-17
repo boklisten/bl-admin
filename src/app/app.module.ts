933ab27562be62c40dd3446bb04b716e6323a9e4
holskil <aholskil@gmail.com>
2018-04-17 09:26:07 +0200

    fix: bootstrap and boklisten logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {LoginModule} from '@wizardcoder/bl-login';
+import {AppRoutingModule} from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import {environment} from '../environments/environment';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    LoginModule.withConfig({successPath: 'home', apiPath: environment.apiPath});
+  }
+}
