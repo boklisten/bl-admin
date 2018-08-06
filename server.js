d0994850b4c83cc1d8adb12aee5beacbfa183e93
holskil <aholskil@gmail.com>
2018-08-06 14:48:40 +0200

    fix: enforce https on http requests

--- server.js
+++ server.js
@@ -3,34 +3,20 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.get('*', (req, res, next) => {
+	if (req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production') {
+		res.redirect('https://' + req.hostname + req.url);
+	} else {
+		next();
+	}
+});
+
 app.use(express.static(__dirname + '/dist'));
 
 // If an incoming request uses
 // a protocol other than HTTPS,
 // redirect that request to the
 // same url but with HTTPS
-
-const forceSSL = function() {
-	return function (req, res, next) {
-		if (req.headers['x-forwarded-proto'] !== 'https') {
-			console.log('the request is http should redirect to https');
-			return res.redirect(
-				['https://', req.get('Host'), req.url].join('')
-			);
-		}
-		next();
-	}
-};
-
-// Instruct the app
-// to use the forceSSL
-// middleware
-
-if (process.env.NODE_ENV === 'production') {
-	console.log('we are in production');
-	app.use(forceSSL());
-}
-
 
 
 // For all GET requests, send back index.html
