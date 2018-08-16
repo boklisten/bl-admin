5594302650ab8a196c3f1707f9e4840144729d54
holskil <aholskil@gmail.com>
2018-08-16 21:12:30 +0200

    fix: removed the use of hash in url

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
+var express = require('express');
+var app = express();
+var path = require('path');
 
-const express = require('express');
-const app = express();
-const path = require('path');
 
-app.get('*', (req, res, next) => {
+
+
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+/*
+app.get('*', function(req, res, next) {
+	if (req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production') {
+		res.redirect('https://' + req.hostname + req.url);
+	} else {
+		next();
+	}
+});
+*/
+/*
+app.get('*', function(req, res, next) {
+	if (req.headers['x-forwarded-proto'] !== 'https') {
+		res.redirect('https://' + req.hostname + req.url);
+	} else {
+		next();
+	}
+});
+*/
+app.get('*', function (req, res, next) {
 	if (req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production') {
 		res.redirect('https://' + req.hostname + req.url);
 	} else {
@@ -13,23 +37,17 @@ app.get('*', (req, res, next) => {
 
 app.use(express.static(__dirname + '/dist'));
 
-// If an incoming request uses
-// a protocol other than HTTPS,
-// redirect that request to the
-// same url but with HTTPS
-
-
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 // use this when you turn off the hash in angular, aka domain/#/endpoint
-
-/*
-app.get('/*', function(req, res) {
-	res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('*', function (req, res) {
+	if (req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production') {
+		res.redirect('https://' + req.hostname + req.url);
+	} else {
+		res.sendFile(path.join(__dirname + '/dist/index.html'));
+	}
 });
 
 
-*/
 
-
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
