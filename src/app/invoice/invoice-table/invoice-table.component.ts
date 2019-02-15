1b105dc83c9821b4a1b43a1bf39ec5a689426bb9
holskil <aholskil@gmail.com>
2019-02-15 17:04:42 +0100

    fix: can sort by invoiceId in invoiceTable

--- src/app/invoice/invoice-table/invoice-table.component.ts
+++ src/app/invoice/invoice-table/invoice-table.component.ts
@@ -5,13 +5,48 @@ import {
 	Input,
 	SimpleChanges,
 	EventEmitter,
-	OnChanges
+	OnChanges,
+	Directive,
+	ViewChildren,
+	QueryList
 } from "@angular/core";
 import { Invoice } from "@wizardcoder/bl-model";
 import { FormControl } from "@angular/forms";
 import { Observable } from "rxjs";
 import { map, startWith } from "rxjs/operators";
 import { InvoiceVismaService } from "../invoice-visma/invoice-visma.service";
+
+export type SortDirection = "asc" | "desc" | "";
+const rotate: { [key: string]: SortDirection } = {
+	asc: "desc",
+	desc: "",
+	"": "asc"
+};
+export const compare = (v1, v2) => (v1 < v2 ? -1 : v1 > v2 ? 1 : 0);
+
+export interface SortEvent {
+	column: string;
+	direction: SortDirection;
+}
+
+@Directive({
+	selector: "th[sortable]",
+	host: {
+		"[class.asc]": 'direction === "asc"',
+		"[class.desc]": 'direction === "desc"',
+		"(click)": "rotate()"
+	}
+})
+export class NgbdSortableHeader {
+	@Input() sortable: string;
+	@Input() direction: SortDirection = "";
+	@Output() sort = new EventEmitter<SortEvent>();
+
+	rotate() {
+		this.direction = rotate[this.direction];
+		this.sort.emit({ column: this.sortable, direction: this.direction });
+	}
+}
 
 @Component({
 	selector: "app-invoice-table",
@@ -28,12 +63,16 @@ export class InvoiceTableComponent implements OnInit, OnChanges {
 	public selectedList: any;
 	public selectedInvoice: Invoice;
 	public printToExcelWait: boolean;
+	public invoicesOrg;
+	public sortByInvoiceIdDirection: "asc" | "desc" | "none";
 
 	constructor(private invoiceVismaService: InvoiceVismaService) {
 		this.selectInvoice = new EventEmitter<Invoice>();
 		this.selectAll = false;
 		this.filter = new FormControl("");
 		this.invoices = [];
+		this.invoicesOrg = [];
+		this.sortByInvoiceIdDirection = "none";
 
 		this.selectedList = {};
 
@@ -47,7 +86,38 @@ export class InvoiceTableComponent implements OnInit, OnChanges {
 
 	ngOnChanges(simpleChanges: SimpleChanges) {
 		if (simpleChanges["invoices"].currentValue) {
+			this.invoicesOrg = simpleChanges["invoices"].currentValue;
 			this.onSelectInvoice(simpleChanges["invoices"].currentValue[0]);
+		}
+	}
+
+	public sortInvoiceId() {
+		if (this.sortByInvoiceIdDirection === "none") {
+			this.sortByInvoiceIdDirection = "asc";
+			this.invoices$ = this.filter.valueChanges.pipe(
+				startWith(""),
+				map(text =>
+					this.invoices.sort(
+						(a, b) => parseInt(b.invoiceId) - parseInt(a.invoiceId)
+					)
+				)
+			);
+		} else if (this.sortByInvoiceIdDirection === "asc") {
+			this.sortByInvoiceIdDirection = "desc";
+			this.invoices$ = this.filter.valueChanges.pipe(
+				startWith(""),
+				map(text =>
+					this.invoices.sort(
+						(a, b) => parseInt(a.invoiceId) - parseInt(b.invoiceId)
+					)
+				)
+			);
+		} else {
+			this.sortByInvoiceIdDirection = "none";
+			this.invoices$ = this.filter.valueChanges.pipe(
+				startWith(""),
+				map(text => this.search(text))
+			);
 		}
 	}
 
