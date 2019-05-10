72c5224cc9bbbc03e9b139bd9ac644f6d8722869
holskil <aholskil@gmail.com>
2019-05-10 12:03:52 +0200

    fix: using option.fresh on customer reload

--- src/app/customer/customer-detail/customer-detail.service.ts
+++ src/app/customer/customer-detail/customer-detail.service.ts
@@ -1,26 +1,35 @@
-import {Injectable} from '@angular/core';
-import {BlApiError, UserDetail} from '@wizardcoder/bl-model';
-import {Subject, Observable} from 'rxjs';
-import {UserDetailService} from '@wizardcoder/bl-connect';
-import {StorageService} from "../../storage/storage.service";
+import { Injectable } from "@angular/core";
+import { BlApiError, UserDetail } from "@wizardcoder/bl-model";
+import { Subject, Observable } from "rxjs";
+import { UserDetailService } from "@wizardcoder/bl-connect";
+import { StorageService } from "../../storage/storage.service";
 
 @Injectable()
 export class CustomerDetailService {
-
 	private _customerDetailChange$: Subject<UserDetail>;
 	private _currentCustomerDetail: UserDetail;
 	private _storageCustomerIdName: string;
 
-	constructor(private _userDetailService: UserDetailService, private _storageService: StorageService) {
-		this._storageCustomerIdName = 'bl-customer-id';
+	constructor(
+		private _userDetailService: UserDetailService,
+		private _storageService: StorageService
+	) {
+		this._storageCustomerIdName = "bl-customer-id";
 		this._customerDetailChange$ = new Subject<UserDetail>();
 
 		if (this._storageService.get(this._storageCustomerIdName)) {
-			this.fetchCustomerDetail(this._storageService.get(this._storageCustomerIdName)).then((customerDetail: UserDetail) => {
-				this.setCustomerDetail(customerDetail);
-			}).catch((error) => {
-				console.log('customerDetailService: could not get the saved customer', error);
-			});
+			this.fetchCustomerDetail(
+				this._storageService.get(this._storageCustomerIdName)
+			)
+				.then((customerDetail: UserDetail) => {
+					this.setCustomerDetail(customerDetail);
+				})
+				.catch(error => {
+					console.log(
+						"customerDetailService: could not get the saved customer",
+						error
+					);
+				});
 		}
 	}
 
@@ -28,51 +37,69 @@ export class CustomerDetailService {
 		if (!this._currentCustomerDetail) {
 			return;
 		}
-		this._userDetailService.getById(this._currentCustomerDetail.id).then((customerDetail: UserDetail) => {
-			this.setCustomerDetail(customerDetail);
-		}).catch((fetchCustomerDetailError) => {
-			console.log('customerDetailService: could not fetch customerDetail');
-		});
+		this._userDetailService
+			.getById(this._currentCustomerDetail.id, { fresh: true })
+			.then((customerDetail: UserDetail) => {
+				this.setCustomerDetail(customerDetail);
+			})
+			.catch(fetchCustomerDetailError => {
+				console.log(
+					"customerDetailService: could not fetch customerDetail"
+				);
+			});
 	}
 
 	public getAndSetCustomerDetailById(id: string): Promise<UserDetail> {
 		return new Promise((resolve, reject) => {
-            this.fetchCustomerDetail(id).then((customerDetail: UserDetail) => {
+			this.fetchCustomerDetail(id).then((customerDetail: UserDetail) => {
+				const obs = this.onCustomerDetailChange().subscribe(() => {
+					obs.unsubscribe();
+					resolve(customerDetail);
+				});
 
-                const obs = this.onCustomerDetailChange().subscribe(() => {
-                    obs.unsubscribe();
-                    resolve(customerDetail);
-                });
-
-                this.setCustomerDetail(customerDetail);
-            });
+				this.setCustomerDetail(customerDetail);
+			});
 		});
 	}
 
 	public fetchCustomerDetail(id: string): Promise<UserDetail> {
-		if (this._currentCustomerDetail && id === this._currentCustomerDetail.id) {
+		if (
+			this._currentCustomerDetail &&
+			id === this._currentCustomerDetail.id
+		) {
 			return Promise.resolve(this._currentCustomerDetail);
 		}
 
-		return this._userDetailService.getById(id).then((customerDetail: UserDetail) => {
-			return customerDetail;
-		}).catch((blApiError: BlApiError) => {
-			throw new Error('customerDetailService: could not fetch customerDetail');
-		});
+		return this._userDetailService
+			.getById(id, { fresh: true })
+			.then((customerDetail: UserDetail) => {
+				return customerDetail;
+			})
+			.catch((blApiError: BlApiError) => {
+				throw new Error(
+					"customerDetailService: could not fetch customerDetail"
+				);
+			});
 	}
 
 	public getCustomerDetail(): UserDetail {
 		return this._currentCustomerDetail;
 	}
 
-	public updateCustomerDetail(patchCustomerDetailObject: any): Promise<UserDetail> {
+	public updateCustomerDetail(
+		patchCustomerDetailObject: any
+	): Promise<UserDetail> {
 		return this._userDetailService
-			.update(this._currentCustomerDetail.id, patchCustomerDetailObject).then((customerDetail: UserDetail) => {
+			.update(this._currentCustomerDetail.id, patchCustomerDetailObject)
+			.then((customerDetail: UserDetail) => {
 				this.setCustomerDetail(customerDetail);
 				return this._currentCustomerDetail;
-		}).catch((blApiError: BlApiError) => {
-			throw new Error('customerDetailService: could not update customerDetails');
-		});
+			})
+			.catch((blApiError: BlApiError) => {
+				throw new Error(
+					"customerDetailService: could not update customerDetails"
+				);
+			});
 	}
 
 	public onCustomerDetailChange(): Observable<UserDetail> {
@@ -86,7 +113,10 @@ export class CustomerDetailService {
 		}
 		*/
 
-		this._storageService.store(this._storageCustomerIdName, customerDetail.id);
+		this._storageService.store(
+			this._storageCustomerIdName,
+			customerDetail.id
+		);
 		this._currentCustomerDetail = customerDetail;
 		this._customerDetailChange$.next(this._currentCustomerDetail);
 	}
@@ -97,7 +127,10 @@ export class CustomerDetailService {
 		}
 
 		for (const key of Object.keys(customerDetail)) {
-			if (!this._currentCustomerDetail[key] || this._currentCustomerDetail[key] !== customerDetail[key]) {
+			if (
+				!this._currentCustomerDetail[key] ||
+				this._currentCustomerDetail[key] !== customerDetail[key]
+			) {
 				return true;
 			}
 		}
@@ -110,5 +143,4 @@ export class CustomerDetailService {
 		this._storageService.remove(this._storageCustomerIdName);
 		this._customerDetailChange$.next();
 	}
-
 }
--- src/app/customer/customer.service.ts
+++ src/app/customer/customer.service.ts
@@ -1,20 +1,22 @@
-import {Injectable} from '@angular/core';
-import {CustomerDetailService} from './customer-detail/customer-detail.service';
-import {Observable, Subject} from 'rxjs';
-import {Customer} from './customer';
-import {CustomerOrderService} from '../order/customer-order/customer-order.service';
-import {Order, OrderItem, UserDetail} from '@wizardcoder/bl-model';
-import {CartService} from '../cart/cart.service';
+import { Injectable } from "@angular/core";
+import { CustomerDetailService } from "./customer-detail/customer-detail.service";
+import { Observable, Subject } from "rxjs";
+import { Customer } from "./customer";
+import { CustomerOrderService } from "../order/customer-order/customer-order.service";
+import { Order, OrderItem, UserDetail } from "@wizardcoder/bl-model";
+import { CartService } from "../cart/cart.service";
 
 @Injectable()
 export class CustomerService {
 	private _customerChange$: Subject<boolean>;
 	private _customer: Customer;
 
-	constructor(private _customerDetailService: CustomerDetailService, private _customerOrderService: CustomerOrderService) {
+	constructor(
+		private _customerDetailService: CustomerDetailService,
+		private _customerOrderService: CustomerOrderService
+	) {
 		this._customerChange$ = new Subject<boolean>();
 		this._customer = null;
-
 		this.handleCustomerDetailChange();
 	}
 
@@ -23,29 +25,31 @@ export class CustomerService {
 	}
 
 	public getCustomerDetail(): UserDetail {
-		return (this._customer) ? this._customer.detail : null;
+		return this._customer ? this._customer.detail : null;
 	}
 
 	public haveCustomer(): boolean {
-		return !(!this._customer);
+		return !!this._customer;
 	}
 
 	public reloadCustomer() {
 		this._customerDetailService.reloadCustomerDetail();
 	}
 
-	public haveOrderedItem(itemId: string): {orderItem: OrderItem, order: Order} {
+	public haveOrderedItem(
+		itemId: string
+	): { orderItem: OrderItem; order: Order } {
 		for (const order of this._customer.orders) {
 			for (const orderItem of order.orderItems) {
-				if (orderItem.item === itemId ) {
-					if (orderItem.type === 'rent' || orderItem.type === 'buy') {
-						return {orderItem: orderItem, order: order};
+				if (orderItem.item === itemId) {
+					if (orderItem.type === "rent" || orderItem.type === "buy") {
+						return { orderItem: orderItem, order: order };
 					}
 				}
 			}
 		}
 
-		throw new Error('customerService: did not have ordered item');
+		throw new Error("customerService: did not have ordered item");
 	}
 
 	public onCustomerChange(): Observable<boolean> {
@@ -72,7 +76,7 @@ export class CustomerService {
 	}
 
 	private setCustomer(detail: UserDetail, orders?: Order[]) {
-		this._customer = {detail: detail, orders: (orders) ? orders : null};
+		this._customer = { detail: detail, orders: orders ? orders : null };
 		this._customerChange$.next(true);
 	}
 }
