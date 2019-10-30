ca8df80d5f438b3cf9e29527da9ffdf8e9bc5a8e
holskil <andreasholskil@protonmail.com>
2019-10-30 16:24:28 +0100

    fix: sort by item title

--- src/app/database/database-items/database-items.component.ts
+++ src/app/database/database-items/database-items.component.ts
@@ -1,36 +1,55 @@
-import {Component, OnInit} from '@angular/core';
-import {ItemService} from '@wizardcoder/bl-connect';
-import {Item} from '@wizardcoder/bl-model';
-import {DatabaseExcelService} from '../database-excel/database-excel.service';
-import {DateService} from '../../date/date.service';
+import { Component, OnInit } from "@angular/core";
+import { ItemService } from "@wizardcoder/bl-connect";
+import { Item } from "@wizardcoder/bl-model";
+import { DatabaseExcelService } from "../database-excel/database-excel.service";
+import { DateService } from "../../date/date.service";
+import { BlcSortService } from "../../bl-common/blc-sort/blc-sort.service";
 
 @Component({
-	selector: 'app-database-items',
-	templateUrl: './database-items.component.html',
-	styleUrls: ['./database-items.component.scss']
+	selector: "app-database-items",
+	templateUrl: "./database-items.component.html",
+	styleUrls: ["./database-items.component.scss"]
 })
 export class DatabaseItemsComponent implements OnInit {
 	public databaseItems: Item[];
 
-	constructor(private _itemService: ItemService, private _databaseExcelService: DatabaseExcelService, private _dateService: DateService) {
+	constructor(
+		private _itemService: ItemService,
+		private _databaseExcelService: DatabaseExcelService,
+		private _dateService: DateService,
+		private blcSortService: BlcSortService
+	) {
 		this.databaseItems = [];
 	}
 
 	ngOnInit() {
-		this._itemService.get().then((items: Item[]) => {
-			this.databaseItems = items;
-		}).catch((err) => {
-			console.log('DatabaseItemsComponent: could not get items', err);
-		});
+		this._itemService
+			.get()
+			.then((items: Item[]) => {
+				this.databaseItems = this.blcSortService.sortByField(
+					items,
+					"title"
+				);
+			})
+			.catch(err => {
+				console.log("DatabaseItemsComponent: could not get items", err);
+			});
 	}
-
 
 	onDownloadItems() {
-		this._itemService.get().then((items: Item[]) => {
-			this._databaseExcelService.objectsToExcelFile(items, 'items_' + this._dateService.currentDateCompact() + '.xlsx');
-		}).catch((err) => {
-			console.log('ItemUploadComponent: could not get the items', err);
-		});
+		this._itemService
+			.get()
+			.then((items: Item[]) => {
+				this._databaseExcelService.objectsToExcelFile(
+					items,
+					"items_" + this._dateService.currentDateCompact() + ".xlsx"
+				);
+			})
+			.catch(err => {
+				console.log(
+					"ItemUploadComponent: could not get the items",
+					err
+				);
+			});
 	}
-
 }
--- src/app/item/item-search/item-search-result/item-search-result.component.ts
+++ src/app/item/item-search/item-search-result/item-search-result.component.ts
@@ -1,35 +1,46 @@
-import {Component, OnInit} from '@angular/core';
-import {Item} from '@wizardcoder/bl-model';
-import {ItemSearchService} from '../item-search.service';
-import {Period} from '@wizardcoder/bl-model/dist/period/period';
-import {BranchItemHelperService} from '../../../branch/branch-item-helper/branch-item-helper.service';
+import { Component, OnInit } from "@angular/core";
+import { Item } from "@wizardcoder/bl-model";
+import { ItemSearchService } from "../item-search.service";
+import { Period } from "@wizardcoder/bl-model/dist/period/period";
+import { BranchItemHelperService } from "../../../branch/branch-item-helper/branch-item-helper.service";
+import { BlcSortService } from "../../../bl-common/blc-sort/blc-sort.service";
 
 @Component({
-	selector: 'app-item-search-result',
-	templateUrl: './item-search-result.component.html',
-	styleUrls: ['./item-search-result.component.scss']
+	selector: "app-item-search-result",
+	templateUrl: "./item-search-result.component.html",
+	styleUrls: ["./item-search-result.component.scss"]
 })
 export class ItemSearchResultComponent implements OnInit {
 	public items: Item[];
 	public notFoundError: string;
 
-	constructor(private _itemSearchService: ItemSearchService, private _branchItemHelperService: BranchItemHelperService) {
-	}
+	constructor(
+		private _itemSearchService: ItemSearchService,
+		private _branchItemHelperService: BranchItemHelperService,
+		private blcSortService: BlcSortService
+	) {}
 
 	ngOnInit() {
 		if (this._itemSearchService.getSearchTerm()) {
-			this.items = this._itemSearchService.getSearchResult();
+			this.items = this.getSearchResult();
 		}
 
 		this._itemSearchService.onSearchResult().subscribe(() => {
-			this.items = this._itemSearchService.getSearchResult();
+			this.items = this.getSearchResult();
 			this.notFoundError = null;
 		});
 
 		this._itemSearchService.onSearchResultError().subscribe(() => {
-			this.notFoundError = 'Could not find any item';
+			this.notFoundError = "Could not find any item";
 			this.items = [];
 		});
+	}
+
+	private getSearchResult(): Item[] {
+		return this.blcSortService.sortByField(
+			this._itemSearchService.getSearchResult(),
+			"title"
+		);
 	}
 
 	isRentValid(item: Item, periodType: Period): boolean {
@@ -43,5 +54,4 @@ export class ItemSearchResultComponent implements OnInit {
 	isSellValid(item: Item) {
 		return this._branchItemHelperService.isSellValid(item);
 	}
-
 }
