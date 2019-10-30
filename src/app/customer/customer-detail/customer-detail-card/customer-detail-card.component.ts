1f594cecae0433e8cdec14f33dc17526099ec886
holskil <andreasholskil@protonmail.com>
2019-10-30 17:06:36 +0100

    feat: show guardian details in customer detail card

--- src/app/customer/customer-detail/customer-detail-card/customer-detail-card.component.ts
+++ src/app/customer/customer-detail/customer-detail-card/customer-detail-card.component.ts
@@ -1,24 +1,31 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {UserDetail} from '@wizardcoder/bl-model';
+import { Component, Input, OnInit } from "@angular/core";
+import { UserDetail } from "@wizardcoder/bl-model";
 
 @Component({
-	selector: 'app-customer-detail-card',
-	templateUrl: './customer-detail-card.component.html',
-	styleUrls: ['./customer-detail-card.component.scss']
+	selector: "app-customer-detail-card",
+	templateUrl: "./customer-detail-card.component.html",
+	styleUrls: ["./customer-detail-card.component.scss"]
 })
 export class CustomerDetailCardComponent implements OnInit {
 	@Input() customerDetail: UserDetail;
 	public customerDetailUpdated: boolean;
+	public showGuardian: boolean;
 
 	constructor() {
 		this.customerDetailUpdated = false;
 	}
 
 	ngOnInit() {
+		this.showGuardian =
+			this.customerDetail.guardian &&
+			this.customerDetail.guardian.name &&
+			this.customerDetail.guardian.name.length > 0;
 	}
 
 	onCustomerDetailUpdated() {
 		this.customerDetailUpdated = true;
+		setTimeout(() => {
+			this.customerDetailUpdated = false;
+		}, 2500);
 	}
-
 }
