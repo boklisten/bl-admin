abbf21dfcb5fd6e57dda9f1c70ceb067cd54af82
holskil <andreasholskil@protonmail.com>
2020-04-24 12:28:45 +0200

    feat: add BookingList

--- src/app/bl-common/pipes/customer/customer-name.pipe.ts
+++ src/app/bl-common/pipes/customer/customer-name.pipe.ts
@@ -1,22 +1,21 @@
-import {Pipe, PipeTransform} from '@angular/core';
-import {UserDetailService} from "@wizardcoder/bl-connect";
-import {UserDetail} from "@wizardcoder/bl-model";
+import { Pipe, PipeTransform } from "@angular/core";
+import { UserDetailService } from "@wizardcoder/bl-connect";
+import { UserDetail } from "@wizardcoder/bl-model";
 
 @Pipe({
-	name: 'customerName'
+	name: "customerName"
 })
 export class CustomerNamePipe implements PipeTransform {
-	
-	constructor(private _userDetailService: UserDetailService) {
-	
-	}
-	
+	constructor(private _userDetailService: UserDetailService) {}
+
 	transform(customerId: string): Promise<string> {
-		return this._userDetailService.getById(customerId).then((customerDetail: UserDetail) => {
-			return (customerDetail.name) ? customerDetail.name : '<no name>';
-		}).catch(() => {
-			return '<no customer>';
-		});
+		return this._userDetailService
+			.getById(customerId)
+			.then((customerDetail: UserDetail) => {
+				return customerDetail.name ? customerDetail.name : "<no name>";
+			})
+			.catch(() => {
+				return "<no customer>";
+			});
 	}
-	
 }
--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -7,16 +7,23 @@ import { BookingCreateComponent } from "./booking-create/booking-create.componen
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { NgbTimepickerModule } from "@ng-bootstrap/ng-bootstrap";
+import { BookingListComponent } from "./booking-list/booking-list.component";
+import { BlCommonModule } from "../bl-common/bl-common.module";
 
 @NgModule({
-	declarations: [BookingComponent, BookingCreateComponent],
+	declarations: [
+		BookingComponent,
+		BookingCreateComponent,
+		BookingListComponent
+	],
 	imports: [
 		CommonModule,
 		BookingRoutingModule,
 		FontAwesomeModule,
 		ReactiveFormsModule,
 		NgbTimepickerModule,
-		FormsModule
+		FormsModule,
+		BlCommonModule
 	]
 })
 export class BookingModule {}
