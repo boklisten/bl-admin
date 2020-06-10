8ac4edc3449bc5a9a1f84105657076c9464205a3
holskil <andreasholskil@protonmail.com>
2020-06-10 12:13:01 +0200

    feat: added support for wait in customerCurrentComponent

--- src/app/customer/customer-current/customer-current.component.ts
+++ src/app/customer/customer-current/customer-current.component.ts
@@ -1,18 +1,21 @@
-import { Component, HostListener, OnInit } from "@angular/core";
+import { Component, HostListener, OnInit, OnDestroy } from "@angular/core";
 
 import { UserDetail } from "@wizardcoder/bl-model";
 import { Router } from "@angular/router";
 import { CustomerService } from "../customer.service";
+import { Subscription } from "rxjs";
 
 @Component({
 	selector: "app-customer-current",
 	templateUrl: "./customer-current.component.html",
 	styleUrls: ["./customer-current.component.scss"]
 })
-export class CustomerCurrentComponent implements OnInit {
+export class CustomerCurrentComponent implements OnInit, OnDestroy {
 	public customerDetail: UserDetail;
 	public lastPopoverRef: any;
 	public wait: boolean;
+	private customer$: Subscription;
+	private customerWait$: Subscription;
 
 	constructor(
 		private _router: Router,
@@ -20,15 +23,13 @@ export class CustomerCurrentComponent implements OnInit {
 	) {}
 
 	ngOnInit() {
-		this.wait = true;
 		this.onCustomerChange();
+		this.onCustomerWaitChange();
 	}
 
-	private onCustomerChange() {
-		this._customerService.subscribe((customerDetail: UserDetail) => {
-			this.customerDetail = customerDetail;
-			this.wait = false;
-		});
+	ngOnDestroy() {
+		this.customer$.unsubscribe();
+		this.customerWait$.unsubscribe();
 	}
 
 	public onClearCustomer() {
@@ -50,6 +51,20 @@ export class CustomerCurrentComponent implements OnInit {
 	public onChangeCustomerDetail() {
 		this._router.navigate(["/search"]);
 		this.lastPopoverRef.close();
+	}
+
+	private onCustomerChange() {
+		this.customer$ = this._customerService.subscribe(
+			(customerDetail: UserDetail) => {
+				this.customerDetail = customerDetail;
+			}
+		);
+	}
+
+	private onCustomerWaitChange() {
+		this.customerWait$ = this._customerService.onWait(wait => {
+			this.wait = wait;
+		});
 	}
 
 	@HostListener("document:click", ["$event"])
