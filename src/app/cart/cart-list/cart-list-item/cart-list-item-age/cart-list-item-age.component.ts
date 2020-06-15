29d8e38ba52c80d5da85c2a3c2b36c235cc4cc16
holskil <andreasholskil@protonmail.com>
2020-06-15 12:09:07 +0200

    fix: re-added age select in cart-list-item

--- src/app/cart/cart-list/cart-list-item/cart-list-item-age/cart-list-item-age.component.ts
+++ src/app/cart/cart-list/cart-list-item/cart-list-item-age/cart-list-item-age.component.ts
@@ -1,36 +1,38 @@
-import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
-import { CartItem } from "../../../cartItem";
+import { Component, OnInit, Input, OnDestroy } from "@angular/core";
+import { CartItem } from "../../../cart-item/cart-item";
+import { CartItemAction } from "../../../cart-item/cart-item-action";
 import { OrderItemHelperService } from "../../../order-item-helper/order-item-helper.service";
+import { Subscription } from "rxjs";
 
 @Component({
 	selector: "app-cart-list-item-age",
 	templateUrl: "./cart-list-item-age.component.html",
 	styleUrls: ["./cart-list-item-age.component.scss"]
 })
-export class CartListItemAgeComponent implements OnInit {
+export class CartListItemAgeComponent implements OnInit, OnDestroy {
 	@Input() cartItem: CartItem;
-	@Output() itemAgeChange: EventEmitter<"new" | "used">;
+	public cartItemAction: CartItemAction;
+	private cartItem$: Subscription;
 
-	constructor(private _orderItemHelperService: OrderItemHelperService) {
-		this.itemAgeChange = new EventEmitter();
+	constructor() {}
+
+	ngOnInit() {
+		this.cartItemAction = this.cartItem.getAction();
+		this.cartItemAction.age = "new";
+		this.handleCartItemChange();
 	}
 
-	ngOnInit() {}
+	ngOnDestroy() {
+		this.cartItem$.unsubscribe();
+	}
 
 	public onAgeChange() {
-		this.itemAgeChange.emit(this.cartItem.orderItem.age);
+		this.cartItem.setAction(this.cartItemAction);
+	}
 
-		this._orderItemHelperService
-			.updateOrderItem(
-				this.cartItem.action,
-				this.cartItem.orderItem,
-				this.cartItem.item,
-				this.cartItem.period,
-				this.cartItem.customerItem
-			)
-			.then(() => {})
-			.catch(e => {
-				console.log("cartListItemAge: could not update orderItem", e);
-			});
+	private handleCartItemChange() {
+		this.cartItem$ = this.cartItem.subscribe(() => {
+			this.cartItemAction = this.cartItem.getAction();
+		});
 	}
 }
