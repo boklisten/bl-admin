360c35bf3f4e03b79011ef0f73c2f35cc7e0d6f8
holskil <andreasholskil@protonmail.com>
2020-09-14 16:26:36 +0200

    fix: blid-scanner to search for blid in unique-item-searchbar

--- src/app/unique-item/unique-item-searchbar/unique-item-searchbar.component.ts
+++ src/app/unique-item/unique-item-searchbar/unique-item-searchbar.component.ts
@@ -1,20 +1,39 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, OnDestroy } from "@angular/core";
 import { Router } from "@angular/router";
+import { BlcScannerService } from "../../bl-common/blc-scanner/blc-scanner.service";
+import { Subscription } from "rxjs";
 
 @Component({
 	selector: "app-unique-item-searchbar",
 	templateUrl: "./unique-item-searchbar.component.html",
 	styleUrls: ["./unique-item-searchbar.component.scss"]
 })
-export class UniqueItemSearchbarComponent implements OnInit {
+export class UniqueItemSearchbarComponent implements OnInit, OnDestroy {
 	@Input() blid: string;
+	private blidScanner: Subscription;
 
-	constructor(private _router: Router) {}
+	constructor(
+		private _router: Router,
+		private _blcScannerService: BlcScannerService
+	) {}
 
 	ngOnInit() {
 		if (!this.blid) {
 			this.blid = "";
 		}
+
+		this.handleBlcScannerChange();
+	}
+
+	ngOnDestroy() {
+		this.blidScanner.unsubscribe();
+	}
+
+	private handleBlcScannerChange() {
+		this.blidScanner = this._blcScannerService.onBlid(blid => {
+			this.blid = blid;
+			this.onBlidSearch();
+		});
 	}
 
 	public onBlidSearch() {
