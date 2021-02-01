5242f55ac1e34caed309d9c98a8d37194515b0a7
AdrianAndersen <adrianandersen@protonmail.com>
2021-02-01 18:53:29 +0100

    build: update test api url

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,7 +2,7 @@
 const { version } = require("../../package.json");
 export const environment = {
 	production: false,
-	apiPath: "http://api.test.boklisten.no/",
+	apiPath: "https://blapi.test.boklisten.no/",
 	hmr: false,
 	version: version,
 	versionName: "DEV"
