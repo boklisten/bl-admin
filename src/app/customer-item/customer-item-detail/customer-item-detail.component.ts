d62920afaa6e001a4641f0c25b565d9876489609
Adrian Andersen <adrianandersen@protonmail.com>
2023-06-20 22:34:52 +0200

    feat: ability to update customer item deadlines

--- src/app/customer-item/customer-item-detail/customer-item-detail.component.ts
+++ src/app/customer-item/customer-item-detail/customer-item-detail.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, Input } from "@angular/core";
 import { ActivatedRoute, Params } from "@angular/router";
 import { CustomerItemService, ItemService } from "@boklisten/bl-connect";
 import { CustomerItem, Item } from "@boklisten/bl-model";
+import moment from "moment-es6";
+import { Moment } from "moment";
+import { ToasterService } from "../../toaster/toaster.service";
+import { AuthService } from "../../auth/auth.service";
 
 @Component({
 	selector: "app-customer-item-detail",
@@ -13,16 +17,21 @@ export class CustomerItemDetailComponent implements OnInit {
 	@Input() customerItemId: string;
 	public item: Item;
 	public showMore: boolean;
+	public deadlineInput: string;
+	public isAdmin: boolean;
 
 	constructor(
 		private _route: ActivatedRoute,
 		private _customerItemService: CustomerItemService,
-		private _itemService: ItemService
+		private _itemService: ItemService,
+		private _toasterService: ToasterService,
+		private _authService: AuthService
 	) {
 		this.showMore = false;
 	}
 
 	ngOnInit() {
+		this.isAdmin = this._authService.isAdmin();
 		if (this.customerItem) {
 			this.getItem();
 		}
@@ -53,7 +62,6 @@ export class CustomerItemDetailComponent implements OnInit {
 				this.getItem();
 			})
 			.catch((getCustomerItemError) => {
-				console.log("hello there", this.getCustomerItem);
 				console.log(
 					"customerItemDetailComponent: could not get customerItem",
 					getCustomerItemError
@@ -61,14 +69,45 @@ export class CustomerItemDetailComponent implements OnInit {
 			});
 	}
 
+	private toPrettyDate(date: Date): string {
+		return moment(date).format("DD.MM.YYYY");
+	}
+
 	private getItem() {
 		this._itemService
 			.getById(this.customerItem.item as string)
 			.then((item: Item) => {
 				this.item = item;
+				this.deadlineInput = this.toPrettyDate(
+					this.customerItem.deadline
+				);
 			})
 			.catch((getItemError) => {
 				console.log("customerItemDetailComponet: could not get item");
 			});
+	}
+
+	public hasChangedDate(): boolean {
+		return (
+			this.toPrettyDate(this.customerItem.deadline) !== this.deadlineInput
+		);
+	}
+
+	public async handleUpdateDeadline() {
+		try {
+			this.customerItem = await this._customerItemService.update(
+				this.customerItem.id,
+				{
+					deadline: moment(this.deadlineInput, "DD.MM.YYYY"),
+				}
+			);
+			this._toasterService.add("SUCCESS", "Fristen ble oppdatert!");
+		} catch (e) {
+			this._toasterService.add("WARNING", {
+				text: "Oppdatering av fristen feilet. Årsak: " + e?.msg,
+			});
+		} finally {
+			this.deadlineInput = this.toPrettyDate(this.customerItem.deadline);
+		}
 	}
 }
