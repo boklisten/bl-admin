34415fe1b56275fd7d649f097c33bf40d82b1867
Adrian Andersen <adrianandersen@protonmail.com>
2023-06-20 22:58:13 +0200

    feat: ability to update order branch

--- src/app/order/order-detail/order-detail-card/order-detail-card.component.ts
+++ src/app/order/order-detail/order-detail-card/order-detail-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from "@angular/core";
-import { Order } from "@boklisten/bl-model";
+import { Branch, Order } from "@boklisten/bl-model";
+import { BranchService, OrderService } from "@boklisten/bl-connect";
+import { ToasterService } from "../../../toaster/toaster.service";
+import { AuthService } from "../../../auth/auth.service";
 
 @Component({
 	selector: "app-order-detail-card",
@@ -9,7 +12,42 @@ import { Order } from "@boklisten/bl-model";
 export class OrderDetailCardComponent implements OnInit {
 	@Input() order: Order;
 
-	constructor() {}
+	public selectedBranch: string;
+	public branches: Branch[];
+	public isAdmin: boolean;
 
-	ngOnInit() {}
+	constructor(
+		private _branchService: BranchService,
+		private _orderService: OrderService,
+		private _toasterService: ToasterService,
+		private _authService: AuthService
+	) {}
+
+	ngOnInit() {
+		this.isAdmin = this._authService.isAdmin();
+		this.getAllBranches();
+		this.selectedBranch = String(this.order.branch);
+		console.log(this.selectedBranch);
+	}
+
+	private async getAllBranches() {
+		this._branchService.get().then((branches: Branch[]) => {
+			this.branches = branches;
+		});
+	}
+
+	public async updateOrderBranch(newBranchId: string) {
+		try {
+			this.order = await this._orderService.update(this.order.id, {
+				branch: newBranchId,
+			});
+			this._toasterService.add("SUCCESS", "Filial ble oppdatert!");
+		} catch (e) {
+			this._toasterService.add("WARNING", {
+				text: "Oppdatering av filial feilet! Årsak: " + e?.msg,
+			});
+		} finally {
+			this.selectedBranch = String(this.order.branch);
+		}
+	}
 }
