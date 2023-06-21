25290a6638b658970a2943ac76dcf6d06490afe7
Adrian Andersen <adrianandersen@protonmail.com>
2023-06-21 08:58:34 +0200

    feat: ability to update order branch (#690)
    * feat: ability to update order branch
    
    * fix(update-order-branch): also update customerItems

--- src/app/order/order-detail/order-detail-card/order-detail-card.component.ts
+++ src/app/order/order-detail/order-detail-card/order-detail-card.component.ts
@@ -1,5 +1,12 @@
 import { Component, Input, OnInit } from "@angular/core";
-import { Order } from "@boklisten/bl-model";
+import { Branch, Order } from "@boklisten/bl-model";
+import {
+	BranchService,
+	CustomerItemService,
+	OrderService,
+} from "@boklisten/bl-connect";
+import { ToasterService } from "../../../toaster/toaster.service";
+import { AuthService } from "../../../auth/auth.service";
 
 @Component({
 	selector: "app-order-detail-card",
@@ -9,7 +16,57 @@ import { Order } from "@boklisten/bl-model";
 export class OrderDetailCardComponent implements OnInit {
 	@Input() order: Order;
 
-	constructor() {}
+	public selectedBranch: string;
+	public branches: Branch[];
+	public isAdmin: boolean;
 
-	ngOnInit() {}
+	constructor(
+		private _branchService: BranchService,
+		private _orderService: OrderService,
+		private _toasterService: ToasterService,
+		private _authService: AuthService,
+		private _customerItemService: CustomerItemService
+	) {}
+
+	ngOnInit() {
+		this.isAdmin = this._authService.isAdmin();
+		this.getAllBranches();
+		this.selectedBranch = String(this.order.branch);
+	}
+
+	private async getAllBranches() {
+		this._branchService.get().then((branches: Branch[]) => {
+			this.branches = branches;
+		});
+	}
+
+	public async updateOrderBranch(newBranchId: string) {
+		try {
+			this.order = await this._orderService.update(this.order.id, {
+				branch: newBranchId,
+			});
+			const customerItems = await this._customerItemService.getManyByIds(
+				this.order.orderItems.map((orderItem) =>
+					String(orderItem.customerItem)
+				)
+			);
+			await Promise.all(
+				customerItems.map((customerItem) =>
+					this._customerItemService.update(customerItem.id, {
+						handoutInfo: {
+							...customerItem?.handoutInfo,
+							handoutById: newBranchId,
+						},
+					})
+				)
+			);
+			this._toasterService.add("SUCCESS", "Filial ble oppdatert!");
+		} catch (e) {
+			this._toasterService.add("WARNING", {
+				text: "Oppdatering av filial feilet! Årsak: " + e?.msg,
+			});
+		} finally {
+			this.selectedBranch = String(this.order.branch);
+		}
+	}
 }
