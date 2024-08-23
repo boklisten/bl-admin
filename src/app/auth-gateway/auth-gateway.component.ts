74440a204852d89ea65c61084a270852bc3e4157
Adrian Andersen <adrianandersen@protonmail.com>
2024-08-23 12:12:55 +0200

    fix(AuthGateway): redirect to permission page if not employee

--- src/app/auth-gateway/auth-gateway.component.ts
+++ src/app/auth-gateway/auth-gateway.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { AuthService } from "../auth/auth.service";
 
 @Component({
 	selector: "app-auth-gateway",
 	templateUrl: "./auth-gateway.component.html",
 })
 export class AuthGatewayComponent implements OnInit {
-	constructor(private router: Router) {}
+	constructor(private router: Router, private authService: AuthService) {}
 
 	ngOnInit(): void {
 		setTimeout(() => {
-			this.router.navigate(["/"], { replaceUrl: true });
+			this.router.navigate(
+				[
+					this.authService.isEmployee()
+						? "/"
+						: "/auth/permission/denied",
+				],
+				{ replaceUrl: true }
+			);
 		}, 200);
 	}
 }
