a799a5a3c425f52d9196526bda957eb816e7e227
Adrian Andersen <adrianandersen@protonmail.com>
2025-11-03 16:16:03 +0100

    fix: update link to next

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,7 +2,7 @@ const { version } = require("../../package.json");
 export const environment = {
 	production: false,
 	apiPath: "https://staging.api.boklisten.no/",
-	nextPath: "https://staging.next.boklisten.no/",
+	nextPath: "https://staging.boklisten.no/",
 	hmr: false,
 	version: version,
 	versionName: "DEV",
