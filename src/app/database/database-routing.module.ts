e60faf3141fe0e47931fcec626acfe8498a398bc
Adrian Andersen <adrianandersen@protonmail.com>
2025-11-04 16:47:57 +0100

    chore: redirect to next for branches page

--- src/app/database/database-routing.module.ts
+++ src/app/database/database-routing.module.ts
@@ -48,7 +48,7 @@ const routes: Routes = [
 			},
 			{
 				path: "branches",
-				component: DatabaseBranchesComponent,
+				component: BlNextLinkerComponent,
 				children: [
 					{
 						path: "",
